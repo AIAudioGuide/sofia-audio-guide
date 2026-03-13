0aa8cf44cb5a4cddd736513da683de9e5f20ad6c
Kris Mitov <krismitov@Kriss-MacBook-Pro.local>
2026-03-13 12:49:13 +0200

    Add 2 waypoints for route 5-6

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -41,6 +41,7 @@ const LANDMARKS = [
     lat: 42.698976854779225, lng: 23.322591427324415,
     viewingPoint: { lat: 42.698976854779225, lng: 23.322591427324415 },
     waypointsToNext: [
+      { lat: 42.69921455389012, lng: 23.322589170873083 },
       { lat: 42.699319956493035, lng: 23.322808284558437 },
     ],
     desc: 'The Square of Tolerance is a unique public space where a mosque, synagogue, and church stand near each other, symbolizing the religious tolerance of Sofia. It is one of the few places in the world where three Abrahamic faiths coexist in such close proximity.',
